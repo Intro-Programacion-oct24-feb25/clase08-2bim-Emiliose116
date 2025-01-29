796f60d9e7247539c1794400e89f6c771b914c63
Emilio J Cueva <ecueva611@gmail.com>
2025-01-29 11:52:28 -0500

    Xnhnnth

--- Ejercicios07/src/lectura01/LeerArchivoTexto.java
+++ Ejercicios07/src/lectura01/LeerArchivoTexto.java
@@ -71,11 +71,11 @@ public class LeerArchivoTexto {
                 String linea = entrada.nextLine();
                 List<String> lista = Arrays.asList(linea.split(";"));
                 ArrayList<String> linea_partes = new ArrayList<>(lista);
-                // for (int i = 0; i < linea_partes.size(); i++) {
-                    System.out.println(linea_partes.get(2));
-                // }
+                 for (int i = 0; i < linea_partes.size(); i++) {
+                    System.out.println(linea_partes.get(i));
+                 }
                 System.out.println("-----------------------------------");
-
+                 
             } // fin de while
             entrada.close();
         } // fin de try
