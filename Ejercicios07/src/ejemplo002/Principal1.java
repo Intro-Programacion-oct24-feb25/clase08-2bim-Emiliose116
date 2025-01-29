1bb51a7102b45a2deb06e0649334e259fcf8658c
Emilio J Cueva <ecueva611@gmail.com>
2025-01-29 12:49:17 -0500

    Xnhnnth

--- Ejercicios07/src/ejemplo002/Principal1.java
+++ Ejercicios07/src/ejemplo002/Principal1.java
@@ -11,11 +11,18 @@ public class Principal1 {
 
     public static void main(String[] args) {
         
-        String [] nombres = { "Jason", "Jonathan", "Kristen", "Robin", "Michelle", "Emily", "Noah", "Daniel"};
-        String [] apellidos = { "Lynch", "George", "Lang", "Cochran", "Young", "Fletcher", "Adkins", "Harris"};
-        int [][] notas = { {10, 80, 80, 95}, {40, 80, 80, 45}, {80, 10, 20, 55}, {70, 30, 20, 65}, 
-            {60, 50, 70, 75}, {50, 70, 30, 85},{40, 80, 40, 45}, {30, 90, 50, 95}};
+        String [] nombres = { "Jason", "Jonathan", "Kristen", "Robin", 
+                              "Michelle", "Emily", "Noah", "Daniel"};
+        String [] apellidos = { "Lynch", "George", "Lang", "Cochran", 
+                                "Young", "Fletcher", "Adkins", "Harris"};
+        int [][] notas = { {10, 80, 80, 95}, {40, 80, 80, 45}, 
+                         {80, 10, 20, 55}, {70, 30, 20, 65}, 
+                         {60, 50, 70, 75}, {50, 70, 30, 85},
+                         {40, 80, 40, 45}, {30, 90, 50, 95}};
         
+        String correo;
+        int notaBaja;
+        int notaAlta;
         double promedio_paralelo = obtenerPromedioParalelo(notas);
         String nombre;
         String apellido;
@@ -28,27 +35,39 @@ public class Principal1 {
             nombre = nombres[i];
             apellido = apellidos[i];
             filaNotas = notas[i];
+            notaBaja = obtenerNotaMasBaja(filaNotas);
+            notaAlta = obtenerNotaMasAlta(filaNotas);
+            correo = obtenerCorreo(nombre, apellido);
             promedioEstudiante = funcion01(filaNotas);
             numeroNotasArribaPromedio = funcion02(filaNotas, 
                     promedio_paralelo);
             tipoNotas = funcion03(filaNotas);
             mensajeFinal = String.format("%s%s\n", mensajeFinal, 
-                    presentarReporte(nombre, apellido, tipoNotas, 
-                    promedioEstudiante, numeroNotasArribaPromedio));
+                    presentarReporte(nombre, apellido, correo,tipoNotas, 
+                    promedioEstudiante, numeroNotasArribaPromedio, notaBaja, 
+                    notaAlta));
+            /*Arriba mandamos dos parametros nomás si nos fiojamos bien dentro 
+            del stringformat estamos llamando una funcion y le estamos mandando 
+            sus respectivos parametros*/
         }
         CrearArchivoTexto.agregarRegistros(mensajeFinal);
-
+        //aqui llamo a la funcion que etsa en otra clase
     }
     
-    public static String presentarReporte(String nom, String ap, String notas, 
-            double prom, int numeroNotas){
+    public static String presentarReporte(String nom, String ap, String correo,
+            String notas, double prom, int numeroNotas, int notaBaja, 
+            int notaAlta){
+        
         String reporte = String.format("Nombres: %s\n"
                 + "Apellidos: %s\n"
+                + "Username: %s\n"
                 + "Con notas: \n"
-                + "%s\n"
+                + "%s"
                 + "Promedio - %2f\n"
-                + "Número de notas arriba del promedio: %d\n\n",
-                nom, ap, notas, prom, numeroNotas);
+                + "Número de notas arriba del promedio: %d\n"
+                + "Nota mas baja: %d\n"
+                + "Nota mas alta: %d\n\n",
+                nom, ap, correo,notas, prom, numeroNotas, notaBaja, notaAlta);
         
         return reporte;
     }
@@ -117,7 +136,38 @@ public class Principal1 {
         return cadena;
     }
     
+    public static String obtenerCorreo(String nom, String ap){
+        String correo;
+        String inicial;
+        String apellidoMinus;
+        
+        inicial = nom.toLowerCase().substring(0, 1);
+        apellidoMinus = ap.toLowerCase();
+        correo = String.format("%s.%s@utpl.edu.ec", inicial, apellidoMinus);
+        return correo;
+    }
     
+    public static int obtenerNotaMasBaja(int [] notas){
+        int NotaBaja = notas[0];
+        int i;
+        
+        for(i = 1;i<notas.length;i++){
+            if(NotaBaja > notas[i]){
+                NotaBaja = notas[i];
+            }
+        }
+        return NotaBaja;
+    }
     
-    
+    public static int obtenerNotaMasAlta(int [] notas){
+        int NotaAlta = notas[0];
+        int i;
+        
+        for(i = 1;i<notas.length;i++){
+            if(NotaAlta < notas[i]){
+                NotaAlta = notas[i];
+            }
+        }
+        return NotaAlta;
+    }
 }
